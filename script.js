948beaae25b7b0ab88dcdd3f3dfcdbb7634efac5
Artem <rogovartemalex@gmail.com>
2019-03-08 14:59:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ document.addEventListener('click', _=>{
     }
     if (counter===3){
     tl
+    .to(text3, 1, {display:'none', x:'-100%'})
     .to(text, 4, {display:'block', x:'-100%'}, 'steel')
-    .to(text3, 2, {display:'none', x:'-100%'})
     .to(hero, 2, {x:'-100%', width:'100%'}, 'steel')
     .to(overlay, 2, {width:'100%'},'steel')
     .to(overlay, 0.5, {width:0, left:0, delay:2}, 'steel')
